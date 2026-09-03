4d3ff7d0d6b06eece66a03f1108d5d77a9990f8d
Lam (Max) Nguyen <lamtnguyen989@gmail.com>
2026-09-03 11:48:04 -0500

    minor scripts changes

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+mkdir -p data/
+mkdir -p data/shards/
+
 python3 scripts/generate_data.py data/vectors.vecf --num-vectors 2000000 --dim 512 --cluster-st-dev 10.5
 python3 scripts/sample.py data/vectors.vecf data/sample.vecf --sample-size 500000
--- scripts/run_training.sh
+++ scripts/run_training.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
 # Just a simple script to automate the training pipeline for developing
+# Make sure you have the Spack environment already activated before running this script
 
-mkdir -p data/
-mkdir -p data/shards/
 make train
-
-mpiexec -n 4 build/train data/sample.vecf data/shards/pq_codebook.pqbook 32 256 100 8 0.0 69
+mpiexec -n 4 build/train data/sample.vecf data/shards/pq_codebook.pqbook 32 256 100 4 0.0 69
