fc373177184286046b51499491ce080e1d506b59
Lam Nguyen <lamtnguyen989@gmail.com>
2026-09-03 17:26:37 -0500

    index and candidates

--- include/index_format.h
+++ include/index_format.h
@@ -4,6 +4,7 @@
 #define INDEX_FORMAT_H
 
 #include <stdint.h>
+#include <stdlib.h>
 
 /* Index book keeping data structures */
 // Header
@@ -21,6 +22,15 @@ typedef struct {
     const uint32_t* neighbors;
 } IndexRecord;
 
+// Transforming byte buffer into an index record
+static inline void index_record_decode(IndexHeader* header, const uint8_t* buffer, IndexRecord* out_record)
+{
+    out_record->vector = (const float*) buffer;
+    const uint32_t *tail = (const uint32_t*) (buffer + (size_t)header->dim * sizeof(float));
+    out_record->degree = tail[0];
+    out_record->neighbors = tail + 1;
+}
+
 
 /* List (essentially a vec or array) of searching candidates */
 // Searching candidates (with respect to a vector)
@@ -36,6 +46,71 @@ typedef struct {
     uint32_t size;
     uint32_t capacity;
 } CandidateList;
+
+static inline void candidate_list_init(CandidateList* list, uint32_t capacity)
+{
+    list->items = (Candidate*) malloc(capacity*sizeof(Candidate));
+    list->size = 0;
+    list->capacity = capacity;
+}
+
+static inline void candidate_list_free(CandidateList *list)
+{
+    free(list->items);
+    list->items = NULL;
+}
+
+// Insert candidate into the list, sorted by ascending distance
+// Returns 1 for success (true) and 0 (false) for fail 
+static inline int insert_candidate(CandidateList *list, uint32_t id, float distance)
+{
+    /* Rejection checks */
+    // Check if the id is already present
+    for (uint32_t i = 0; i < list->size; i++) {
+        if (list->items[i].id == id) {
+            return 0;
+        }
+    }
+    // Reject inserting candidate if the list is full and the candidates is worse than the worst candidate
+    if ((list->size == list->capacity) && (list->items[list->size - 1].dist <= distance)) {
+        return 0;
+    }
+
+    /* Insertion */
+    // Finding insertion point
+    uint32_t last_pos = (list->size < list->capacity) ? (list->size) : (list->capacity - 1);
+    uint32_t insert_pos = 0;
+    while ((insert_pos < last_pos) && list->items[insert_pos].dist < distance) {
+        insert_pos++;
+    }
+    
+    // Insert to the end of the list 
+    for (uint32_t i = last_pos; i > insert_pos; i--) {
+        list->items[i] = list->items[i - 1];
+    }
+    list->items[insert_pos] = (Candidate){
+        .id = id,
+        .dist = distance,
+        .visited = 0,
+    };
+
+    // Increase the size after successfully insert
+    if (list->size < list->capacity) {
+        list->size++;
+    }
+
+    return 1;
+}
+
+static inline uint32_t next_unvisted_candidates(CandidateList* list, uint32_t* out_indices, uint32_t max_out) {
+    uint32_t count = 0;
+    for (uint32_t i = 0; i < list->size && count < max_out; i++) {
+        if (!list->items[i].visited) {
+            out_indices[count++] = i;
+        }
+    }
+    return count;
+}
 
 
 
