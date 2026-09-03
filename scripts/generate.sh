82f25ecbb4e5b54d43bbaf6d81d8e080ca49db24
Lam (Max) Nguyen <lamtnguyen989@gmail.com>
2026-09-03 12:45:24 -0500

    minor scripts changes

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -3,5 +3,9 @@
 mkdir -p data/
 mkdir -p data/shards/
 
+# Generate and sample training data
 python3 scripts/generate_data.py data/vectors.vecf --num-vectors 2000000 --dim 512 --cluster-st-dev 10.5
 python3 scripts/sample.py data/vectors.vecf data/sample.vecf --sample-size 500000
+
+# Shard
+python3 scripts/shard_vecfile.py data/vectors.vecf data/shards --num-shards 4
