aa60bf3a390f562f1d8d7226ac2e039a07358a0f
Lam (Max) Nguyen <lamtnguyen989@gmail.com>
2026-09-06 23:06:15 -0500

    start with combining build index and encodings

---
+++ src/vamana_pq.c
@@ -0,0 +1,10 @@
+// Building Vamana index and PQ encode shards all in a single MPI executable
+
+#include <stdio.h>
+#include <mpi.h>
+#include <omp.h>
+
+int main(int argc, char** argv) 
+{
+    return 0;
+}
