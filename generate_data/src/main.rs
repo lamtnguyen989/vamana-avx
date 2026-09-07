81a865e8683a31f9342bb77f943f81772a6ff444
Lam (Max) Nguyen <lamtnguyen989@gmail.com>
2026-09-07 15:07:52 -0500

    setting up rayon threadpool

--- generate_data/src/main.rs
+++ generate_data/src/main.rs
@@ -1,13 +1,15 @@
-use std::path::{Path, PathBuf};
+use std::{io::{BufWriter, Write}, path::{Path, PathBuf}};
 
 use clap::Parser;
 use rayon::prelude::*;
 
+const WRITE_BUFFER_SIZE: usize = 16 * 1024 * 1024;   // 8 MB for now
+
 #[derive(Parser, Debug)]
 struct Args 
 {
-    /// Output path: (Internally default to <PROJECT_ROOT>/data/<output_path>)
-    out_path: PathBuf, 
+    /// Output path: [Internally default relative paths to <PROJECT_ROOT>/<output_file>]
+    output_file: PathBuf, 
 
     /// Total number of vectors to be generated
     #[arg(long = "n-vectors", default_value_t = 50_000)]
@@ -24,13 +26,17 @@ struct Args
     /// Standard deviations for each vector blobs
     #[arg(long = "cluster-std-dev", default_value_t = 1.5)] 
     cluster_std_dev: f64, 
+
+    /// Number of threads to be used in generation concurrently
+    #[arg(long = "threads", default_value_t = 4)] 
+    threads: usize, 
     
     /// RNG seed
     #[arg(long, default_value_t = 69)] 
     seed: u64, 
     
     /// Batch size for vector writes
-    #[arg(long, default_value_t = 1000)] 
+    #[arg(long, default_value_t = 2000)] 
     batch_size: usize, 
 }
 
@@ -44,15 +50,31 @@ fn resolve_output_path(user_path: &Path) -> std::io::Result<PathBuf> {
     }
 
     // Relative path processing
-    // Note the default is to write to <PROJECT_ROOT>/data/<data_name>.vecf
+    // Note the default is to write to <PROJECT_ROOT>/<output_file>
     let crate_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
     let project_root = crate_dir.parent().unwrap_or(&crate_dir);
+    let full_path = project_root.join(user_path);
 
-    let data_dir = project_root.join("data");
-    std::fs::create_dir_all(&data_dir)?;
+    // Create all path leading up to the `user_path`
+    if let Some(parent) = full_path.parent() {
+        std::fs::create_dir_all(parent)?;
+    }
 
-    let filename = user_path.file_name().unwrap_or(user_path.as_os_str());
-    Ok(data_dir.join(filename))
+    Ok(full_path)
+}
+
+fn generate_vecfile(args: &Args) -> std::io::Result<()> {
+    // Open up file for serializing
+    let vecfile = std::fs::File::create(&args.output_file)?;
+
+    // Initialize the buffered writer (mmap possibility later but now safety first)
+    let mut writer = BufWriter::with_capacity(WRITE_BUFFER_SIZE, vecfile);
+
+    // Writing header
+    writer.write_all(&(args.num_vectors as u32).to_le_bytes())?;
+    writer.write_all(&(args.dim as u32).to_le_bytes())?;
+
+    todo!();
 }
 
 fn main() -> std::io::Result<()> {
@@ -60,12 +82,22 @@ fn main() -> std::io::Result<()> {
     let mut args = Args::parse();
 
     // Resolving output path
-    args.out_path = resolve_output_path(&args.out_path)?;
+    args.output_file = resolve_output_path(&args.output_file)?;
+
+    // Setting up Rayon threadpool
+    let rayon_setup = rayon::ThreadPoolBuilder::new()
+                        .num_threads(args.threads)
+                        .build_global();
+    match rayon_setup {
+        Ok(_) => println!("Global threadpool initialized successfully."),
+        Err(e) => panic!("Failed to Initialize global threadpool. Error: {}", e)
+    }
 
     // Generate data and serialize to file
+    generate_vecfile(&args)?;
 
 
-    println!("Wrote data to {:?}", args.out_path);
+    println!("Wrote data to {:?}", args.output_file);
 
     Ok(())
 }
