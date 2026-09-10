ec77cd023d4698e2b44ece6caebd2787f8779f9f
Lam (Max) Nguyen <lamtnguyen989@gmail.com>
2026-09-10 15:13:54 -0500

    added full-precision rerankings

--- scripts/ground_truths.sh
+++ scripts/ground_truths.sh
@@ -9,4 +9,4 @@ mkdir -p data/result data/gt
 
 
 # Current still bootstrapping logic with python
-python3 queries/query.py data/vectors.vecf data/queries.vecf data/gt/ground_truths.csv --k 20 --std-dev 0.5
+python3 queries/query.py data/vectors.vecf data/queries.vecf data/gt/ground_truths.csv --k 100 --std-dev 0.5
--- scripts/search.sh
+++ scripts/search.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 make search
-mpiexec -n 4 build/search data/queries.vecf 20 data/shards/index data/shards/pq_codebook.pqbook data/shards/encodings data/result/results.csv 256 64 4
+mpiexec -n 4 build/search data/queries.vecf 100 data/shards/index data/shards/pq_codebook.pqbook data/shards/encodings data/result/results.csv 256 64 4
